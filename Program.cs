873d9ae81e822e32684b19351926bdd0a1c38f78
Finn-McP <fimc673@student.oaisd.org>
2025-12-03 18:59:06 +0000

    Started the boot-up sequence, added wait and a loading bar

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Clear();
+        introTimer();
+    }
+    public static void beginningThing()
+    {
+        
+    }
+    public static void introTimer()
+    {
+        Console.Write("""
+        |    /\    |   _______       _      _
+        |   /  \   |  |_|     |     |_|   _| |_
+        |  /    \  |    ______|      _   |_   _|
+        | /      \ |   /     _|     | |    | |
+        |/        \|   \____/ |_|   |_|    |_|
+        """);
+        Thread.Sleep(4000);
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.Write("Loading ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        for(int i = 1; i <=45; i++)
+        {
+            Console.Write("▅");
+            Thread.Sleep(50);
+        }
+    }
+}
