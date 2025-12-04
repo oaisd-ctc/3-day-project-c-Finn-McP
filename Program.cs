b177d620ae1f732741bcef1129e19be52245e628
Finn-McP <fimc673@student.oaisd.org>
2025-12-04 18:59:47 +0000

    Completly finished the intro sequence.

--- Program.cs
+++ Program.cs
@@ -12,30 +12,41 @@ public class Program
     }
     public static void beginningThing()
     {
-        Thread.Sleep(2000);
         Console.ForegroundColor = ConsoleColor.White;
-        Console.WriteLine("What would you like to name your character?");
-        string userName = Console.ReadLine();
+        Console.Clear();
+        Thread.Sleep(1000);
+        Console.WriteLine("*As you walk near the town borders you see a man*\n");
         Console.ForegroundColor = ConsoleColor.Magenta;
-        Console.WriteLine("Jeff: Welcome traveler, it appears you have stumbled upon our humbled abode.");
+        Thread.Sleep(1000);
+        Console.WriteLine("Jeff: Welcome traveler, it appears you have stumbled upon our humbled abode.\n");
         Console.ForegroundColor = ConsoleColor.Gray;
         Console.WriteLine("1. Talk to Jeff       2. Stare at Jeff");
         Console.WriteLine("3. Talk to someone else      4. Leave");
         Console.ForegroundColor = ConsoleColor.Cyan;
         string userInput = Console.ReadLine();
-        
-        if(userInput == "1")
+
+        if (userInput == "1")
         {
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine($"{userName}: Hello Jeff, what does this place have here?");
+            Console.WriteLine("User: Hello Jeff, what does this place have here?\n");
             Thread.Sleep(2000);
             Console.ForegroundColor = ConsoleColor.Magenta;
             Console.WriteLine("Jeff: Ah yes, you would like to know more about this place.");
             Thread.Sleep(2000);
+            Console.WriteLine("Before I talk more to you, give me your name");
+            Thread.Sleep(1000);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("What would you like to name your character?");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Your name {userName}");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Thread.Sleep(1500);
+            Console.WriteLine($"Jeff: Amazing name {userName}");
+            Thread.Sleep(1500);
             Console.WriteLine("This beautiful town is home to many people, including monsters.");
             Thread.Sleep(2000);
-            Console.WriteLine("Here, I can show you around my house if you would like.");
+            Console.WriteLine("Here, if you want to undetstand this better, I can show you around my house.\n");
             Thread.Sleep(2000);
             Console.ForegroundColor = ConsoleColor.Gray;
             Console.WriteLine("1. Continue with Jeff        2. Talk to someone else");
@@ -43,10 +54,10 @@ public class Program
             Console.ForegroundColor = ConsoleColor.Cyan;
             string userJeff = Console.ReadLine();
 
-            if(userJeff == "1");
+            if (userJeff == "1")
             {
                 Console.Clear();
-                Thread.Sleep(2000);
+                Thread.Sleep(1000);
                 doorOpen();
                 Thread.Sleep(250);
                 Console.Clear();
@@ -65,8 +76,112 @@ public class Program
                 doorOpen6();
                 Thread.Sleep(250);
                 Console.Clear();
-                Console.WriteLine("HEHE");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("*As you enter the house, you notice a machine with a big display screen on it.*\n");
+                Thread.Sleep(2000);
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Jeff: Here is THE BOX!\n");
+                Thread.Sleep(2000);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine($"{userName}: What does this box do?\n");
+                Thread.Sleep(2000);
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Thread.Sleep(2000);
+                Console.WriteLine("Jeff: I'm glad you asked! This box is a new technology that has revolutionized the game of DILMON");
+                Thread.Sleep(2000);
+                Console.WriteLine("Before I go into more detail, he is something cool.");
             }
+            else if(userJeff == "2")
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("*Off to your left you notice an older gentleman who looks like Dumbledore*");
+                Thread.Sleep(2000);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("User: Hi sir, do you have any knowledge that you can tell me?");
+                Thread.Sleep(2000);
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Albus: Uhh idk, Don't die?");
+                Thread.Sleep(2000);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("User: Are you being seirous");
+                Thread.Sleep(4000);
+                beginningThing();
+            }
+            else if(userJeff == "3")
+            {
+                Console.Clear();
+                Thread.Sleep(2000);
+                Console.WriteLine("You choose to end your Dilmon run");
+            }
+        }
+        else if (userInput == "2")
+        {
+            Console.Clear();
+            Thread.Sleep(2000);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("*You begin staring at Jeff for a prolonged period of time.*\n");
+            Thread.Sleep(2000);
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Jeff: Sir, I don't like how you are staring at me.");
+            Thread.Sleep(2000);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("*Jeff is getting angry*\n");
+            Thread.Sleep(2000);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("User: Wow man, its not that serious.\n");
+            Thread.Sleep(2000);
+
+            Random random = new Random();
+            int jeffRandom = random.Next(1,3);
+
+            if(jeffRandom == 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Ok, I guess.");
+                Thread.Sleep(4000);
+                beginningThing();
+            }
+            else if(jeffRandom == 2)
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Ok, that's is it man!");
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Thread.Sleep(2000);
+                Console.WriteLine("*Jeff proceeds to summon {insert Dilmon}, Jeff ends your run *");
+            }
+        }
+        else if (userInput == "3")
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("*Off to the right you notice an older gentleman who looks like Gandalf*\n *Maybe he has some ancient wisdom*");
+            Thread.Sleep(2000);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("User: Hello sir, what knowledge can I consume through your wisdom?\n");
+            Thread.Sleep(2000);
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write("Fladnag: Uh sir");
+            dotTimer();
+            Console.WriteLine("I hope you do realize I'm not intelligent\n");
+            Thread.Sleep(2000);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("User: What do you mean? You look like Gandalf.\n");
+            Thread.Sleep(2000);
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("Fladnag: Just because I look like Gandalf and Fladnag is Gandalf backwards doesn't mean I'm intelligent.");
+            Thread.Sleep(2000);
+            Console.WriteLine("Fladnag: I'm just joking! \n My ancient wisdom is");
+            Thread.Sleep(2000);
+            Console.WriteLine("Drink Water :)");
+            Thread.Sleep(4000);
+            beginningThing();
+
+        }
+        else if (userInput == "4")
+        {
+            Console.Clear();
+            Console.WriteLine("You choose to end your Dilmon run.");
         }
     }
     public static void introTimer()
@@ -84,7 +199,7 @@ public class Program
         Console.ForegroundColor = ConsoleColor.Gray;
         Console.Write("Loading ");
         Console.ForegroundColor = ConsoleColor.Green;
-        for(int i = 1; i <=100; i++)
+        for (int i = 1; i <= 100; i++)
         {
             Console.Write("▅");
             Thread.Sleep(50);
@@ -188,7 +303,7 @@ public class Program
     }
     public static void dotTimer()
     {
-        for(int dot = 1; dot <=3; dot++)
+        for (int dot = 1; dot <= 3; dot++)
         {
             Console.Write(" . ");
             Thread.Sleep(1500);
